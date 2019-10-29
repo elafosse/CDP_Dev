a4f150086c28853467475cb86bc9489575aa054e
elafosse <48483343+elafosse@users.noreply.github.com>
2019-10-29 16:46:24 +0100

    added require mysql

--- cdp-gr1-eq7/backend/index.js
+++ cdp-gr1-eq7/backend/index.js
@@ -2,10 +2,13 @@
 const express = require('express')
 const app = express()
 
+/* REQUIRED */
 const path = require('path')
 const ejs = require('ejs')
 let bodyParser = require('body-parser')
+const mysql = require('mysql')
 
+/* USE THE REQUIRES */
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs')
