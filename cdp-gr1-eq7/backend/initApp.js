174b391f5612869c5d05368986d872f2a2de137f
elafosse <48483343+elafosse@users.noreply.github.com>
2019-11-05 21:51:05 +0100

    fixed /list-projects

--- cdp-gr1-eq7/backend/initApp.js
+++ cdp-gr1-eq7/backend/initApp.js
@@ -7,10 +7,12 @@ const path = require('path')
 const ejs = require('ejs')
 let bodyParser = require('body-parser')
 const index = require('./index')
+const listProjects = require('./listProjects') // TODO : move to signIn
 
 /* USE THE REQUIRES */
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(index.app)
+app.use(listProjects.app) // TODO : move to signIn
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, './..', '/views'))
