a1d17b1ac244d2b828d4e8747fb8f5b009d9ba1d
RayanKheloufi <pikaray00@gmail.com>
2019-11-05 23:12:21 +0100

    Add BD functions

--- cdp-gr1-eq7/backend/bd_connexion.js
+++ cdp-gr1-eq7/backend/bd_connexion.js
@@ -1,6 +1,9 @@
 var mysql = require('mysql');
 var bcrypt = require('bcrypt');
 
+const Task = require('./classes/Task').default;
+
+
 var con = mysql.createConnection({
     host: "www.remotemysql.com",
     user: "wjJ627V9qY",
@@ -11,8 +14,9 @@ var con = mysql.createConnection({
 
 
 // ================ Projects ================
-function createProject(name, description) {
-    let sql = "INSERT INTO project (name, description) VALUES (".concat("'", name, "'", ',', "'", description, "'",')');
+
+export function createProject(name, description) {
+    let sql = "INSERT INTO project (name, description) VALUES (".concat("'", name, "'", ',', "'", description, "'", ')');
     console.log(sql);
     con.query(sql, function (err, result) {
         if (err) throw err;
@@ -20,7 +24,7 @@ function createProject(name, description) {
     });
 }
 
-function deleteProject(username, id){
+export function deleteProject(username, id) {
     //TODO: check if username is the project admin. 
     let sql = "DELETE FROM project WHERE id = ".concat("'", id, "'");
     con.query(sql, function (err, result) {
@@ -29,112 +33,140 @@ function deleteProject(username, id){
     });
 }
 
+export function inviteMembersToProject(projectId, usernameList, areAdminsList) {
+    if (usernameList.length != areAdminsList.length) {
+        throw "The usernameList and the areAdminsList lenght must be the same";
+    }
+    let i;
+    let sql = "";
+    for (i = 0; i < usernameList.length; i++) {
+        sql = sql.concat("INSERT INTO project_team (project_id, username, is_admin) VALUES ('",
+            projectId, "','", usernameList[i], "'", ',', areAdminsList[i], ');\n');
 
-function inviteMembersToProject(projectId, usernameList, areAdminsList) {
-	if(usernameList.length != areAdminsList.length){
-		throw "The usernameList and the areAdminsList lenght must be the same";
-	}
-	let i;
-	let sql = "";
-	for (i = 0; i < usernameList.length; i++) { 
-		sql = sql.concat("INSERT INTO project_team (project_id, username, is_admin) VALUES ('",
-		projectId, "','", usernameList[i], "'", ',', areAdminsList[i], ');\n');
-
-	}
-	console.log(sql);
-	con.query(sql, function (err, result) {
-	if (err) throw err;
-	console.log("Member ".concat(usernameList[i], ' added to project'));
-	});
+    }
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Member ".concat(usernameList[i], ' added to project'));
+    });
 }
 
-function deleteMembersFromProject(projectId, usernameList) {
-	let i;
-	let sql = "";
-	for (i = 0; i < usernameList.length; i++) { 
-		//TODO: check if the user is not the admin of the project
-		sql = sql.concat("DELETE FROM project_team WHERE project_id = ",
-		projectId, " and username = ", "'", usernameList[i], "';\n");
-	}
-	console.log(sql);
-	con.query(sql, function (err, result) {
-	    if (err) throw err;
-	    console.log("Member ".concat(usernameList[i], ' removed from project'));
-	});
+export function deleteMembersFromProject(projectId, usernameList) {
+    let i;
+    let sql = "";
+    for (i = 0; i < usernameList.length; i++) {
+        //TODO: check if the user is not the admin of the project
+        sql = sql.concat("DELETE FROM project_team WHERE project_id = ",
+            projectId, " and username = ", "'", usernameList[i], "';\n");
+    }
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Member ".concat(usernameList[i], ' removed from project'));
+    });
 }
 
-function getUsernamesOfProject(project_id){
-	let sql = "SELECT username FROM project_team WHERE project_id = '"
-		.concat(project_id, "\'");
-		console.log(sql);
-		con.query(sql, function (err, result) {
-		    	if (err) throw err;
-			let id_list = [];
-			for(let i = 0; i < result.length; i++){
-				id_list.push(result[i].username);
-			}
-			console.log(id_list);
-	      		return id_list;
-		});
+//TODO : This must return Member object instead of usernames;
+export function getMembersOfProject(project_id) {
+    let sql = "SELECT username FROM project_team WHERE project_id = '"
+        .concat(project_id, "\'");
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        let id_list = [];
+        for (let i = 0; i < result.length; i++) {
+            id_list.push(result[i].username);
+        }
+        console.log(id_list);
+        return id_list;
+    });
 }
-  
+
 
 // ================ Members ================
-
 //https://medium.com/@mridu.sh92/a-quick-guide-for-authentication-using-bcrypt-on-express-nodejs-1d8791bb418f
 
-
-function storeMember(username, password) {
-    bcrypt.hash(password, 10, function(err, hashedPassword) {
+export function storeMember(username, password) {
+    bcrypt.hash(password, 10, function (err, hashedPassword) {
         if (err) throw err;
-        let sql = "INSERT INTO member (username, password) VALUES (".concat("'", username, "','", hashedPassword, "'",')');
+        let sql = "INSERT INTO member (username, password) VALUES (".concat("'", username, "','", hashedPassword, "'", ')');
         console.log(sql);
         con.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("New member stored : ".concat(username));
+            if (err) throw err;
+            console.log("New member stored : ".concat(username));
         });
     });
 }
 
-function getProjectsIdsOfMember(username) {
-	// TODO: Vérifier si le couple user/project_id n'existe pas déjà
+export function getProjectsIdsOfMember(username) {
+    // TODO: Vérifier si le couple user/project_id n'existe pas déjà
     let sql = "SELECT project_id FROM project_team WHERE username = '"
         .concat(username, "\'");
-        console.log(sql);
-        con.query(sql, function (err, result) {
-            	if (err) throw err;
-		let id_list = [];
-		for(let i = 0; i < result.length; i++){
-			id_list.push(result[i].project_id);
-		}
-		console.log(id_list);
-      		return id_list;
-        });
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        let id_list = [];
+        for (let i = 0; i < result.length; i++) {
+            id_list.push(result[i].project_id);
+        }
+        console.log(id_list);
+        return id_list;
+    });
 }
 
-function getTaskIdsOfMember(username) {
-	// TODO: Vérifier si le couple user/project_id n'existe pas déjà
+export function getTaskIdsOfMember(username) {
+    // TODO: Vérifier si le couple user/project_id n'existe pas déjà
     let sql = "SELECT task_id FROM assigned_task WHERE username = '"
         .concat(username, "\'");
-        console.log(sql);
-        con.query(sql, function (err, result) {
-            	if (err) throw err;
-		let id_list = [];
-		for(let i = 0; i < result.length; i++){
-			id_list.push(result[i].task_id);
-		}
-		console.log(id_list);
-      		return id_list;
-        });
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        let id_list = [];
+        for (let i = 0; i < result.length; i++) {
+            id_list.push(result[i].task_id);
+        }
+        console.log(id_list);
+        return id_list;
+    });
 }
 
-getTaskIdsOfMember("User4");
+export function areUsernameAndPasswordCorrect(username, password, functionSuccess, functionFail) {
+    let sql = "SELECT password FROM member WHERE username = '"
+        .concat(username, "\'");
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) return err;
+        let hashedPassword = result[0].password;
+        bcrypt.compare(password, hashedPassword, function (err, result) {
+            if (result == true) {
+                //True
+                functionSuccess();
+            }
+            else {
+                //False
+                functionFail();
+            }
+        });
+    });
+}
+
+export function deleteMember(username) {
+    let sql = "DELETE FROM member WHERE username = '"
+        .concat(username, "\'");
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Member deleted");
+    });
+    
+}
+
+deleteMember("User4");
 
 // ================ Issues ================
 
-function addIssueToProject(projectId, name, description, priority, difficulty) {
+export function addIssueToProject(projectId, name, description, priority, difficulty) {
     let sql = "INSERT INTO issue (project_id, name, description, priority, difficulty) VALUES ("
-    .concat("'", projectId, "','", name, "','", description, "','", priority, "','", difficulty, ")");
+        .concat("'", projectId, "','", name, "','", description, "','", priority, "','", difficulty, ")");
     console.log(sql);
     con.query(sql, function (err, result) {
         if (err) throw err;
@@ -142,102 +174,173 @@ function addIssueToProject(projectId, name, description, priority, difficulty) {
     });
 }
 
-function getAllProjectIssues(project_id){
-	let sql = "SELECT id FROM issue WHERE project_id = '"
-		.concat(project_id, "\'");
-		console.log(sql);
-		con.query(sql, function (err, result) {
-		    	if (err) throw err;
-			let id_list = [];
-			for(let i = 0; i < result.length; i++){
-				id_list.push(result[i].id);
-			}
-			console.log(id_list);
-	      		return id_list;
-		});
+export function modifyIssue(issueId, name, description, priority, difficulty) {
+    var sql = "UPDATE issue SET".concat(
+        " name = '", name, "',",
+        " description = '", description, "',",
+        " priority = '", priority, "',",
+        " difficulty = '", difficulty, "'",
+        " WHERE id = '", issueId, "';\n");
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result.affectedRows + " record(s) updated");
+    });
 }
 
-function deleteIssue(issueId) {
-	let sql = "DELETE FROM issue WHERE id = '"
-	.concat(issueId, "'");
-	console.log(sql);
-	con.query(sql, function (err, result) {
-	    if (err) throw err;
-	    console.log("Issue removed");
-	});
+export function getAllProjectIssues(project_id) {
+    let sql = "SELECT id FROM issue WHERE project_id = '"
+        .concat(project_id, "\'");
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        let id_list = [];
+        for (let i = 0; i < result.length; i++) {
+            id_list.push(result[i].id);
+        }
+        console.log(id_list);
+        return id_list;
+    });
+}
+
+export function deleteIssue(issueId) {
+    let sql = "DELETE FROM issue WHERE id = '"
+        .concat(issueId, "'");
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Issue removed");
+    });
 }
 
 
 
 // ================ Tasks ================
 
-function addTask(projectId, name, description, state, date_beginning, realisation_time, DoD, dependancies /*list int*/,members /*list User*/) {
-    	let sql = "INSERT INTO task (project_id, name, description, state, start_date, realisation_time, description_of_done) VALUES ("
-	.concat(
-	"'", projectId, "'", ',', 
-	"'", name, "'", ',',
-	"'", description, "'", ',',
-	"'", state, "'", ',',
-	"'", date_beginning, "'", ',',
-	"'", realisation_time, "'", ',',
-	"'", DoD, "'" , ');');
-	console.log(sql);
-	con.query(sql, function (err, result) {
-	if (err) throw err;
-	console.log("New task added");
-	});
-}
-//addTask(2, "Task1", "Blablablabla", "DOING", "2019-05-05", 10, "DoD is a lot");
+export function addTask(projectId, name, description, state, date_beginning, realisation_time, DoD, dependencies /*list int*/, members /*list User*/) {
+    let sql = "INSERT INTO task (project_id, name, description, state, start_date, realisation_time, description_of_done) VALUES ("
+        .concat(
+            "'", projectId, "'", ',',
+            "'", name, "'", ',',
+            "'", description, "'", ',',
+            "'", state, "'", ',',
+            "'", date_beginning, "'", ',',
+            "'", realisation_time, "'", ',',
+            "'", DoD, "'", ');');
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("New task added");
+        let taskId = result.insertId;
+        // TODO: check if the members are part of the project and members/dependencies != [] 
 
-function setTaskDependencies(taskId, dependsOnTasksIdList){
-	var sql = "";
-	for(let i = 0; i < dependsOnTasksIdList.length; i++){
-		sql = sql.concat("INSERT INTO task_dependencies (task_id, depend_on_task_id) VALUES (",
-		"'", taskId, "'", ',',
-		"'", dependsOnTasksIdList[0], "'", '); \n' );
-	}
-	console.log(sql);
-	con.query(sql, function (err, result) {
-	if (err) throw err;
-	console.log("New dependencies added");
-	});
+        setTaskDependencies(taskId, dependencies);
+        setTaskToMembers(taskId, members)
+    });
 }
 
-function setTaskToMembers(taskId, usernameList) {
-	let i;
-	let sql = "";
-	for (i = 0; i < usernameList.length; i++) { 
-		sql = sql.concat("INSERT INTO assigned_task (task_id, username) VALUES ('",
-		taskId, "','", usernameList[i], "'",');\n');
-
-	}
-	console.log(sql);
-	con.query(sql, function (err, result) {
-	if (err) throw err;
-	console.log("Task assigned to members");
-	});
+export function modifyTask(taskId, name, description, state, startDate, realisationTime, DoD) {
+    var sql = "UPDATE task SET".concat(
+        " name = '", name, "',",
+        " description = '", description, "',",
+        " state = '", state, "',",
+        " start_date = '", startDate, "',",
+        " realisation_time = '", realisationTime, "',",
+        " description_of_done = '", DoD, "'",
+        " WHERE id = '", taskId, "';\n");
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result.affectedRows + " record(s) updated");
+    });
 }
 
-function getTaskDependencies(taskId) {
-	// TODO: Vérifier si le couple user/project_id n'existe pas déjà
-    let sql = "SELECT depend_on_task_id FROM task_dependancies WHERE task_id = '"
+export function setTaskDependencies(taskId, dependsOnTasksIdList) {
+    var sql = "DELETE FROM task_dependencies WHERE task_id = '".concat(taskId, "';\n");
+    for (let i = 0; i < dependsOnTasksIdList.length; i++) {
+        sql = sql.concat("INSERT INTO task_dependencies (task_id, depend_on_task_id) VALUES (",
+            "'", taskId, "'", ',',
+            "'", dependsOnTasksIdList[i], "'", '); \n');
+    }
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("New dependencies added");
+    });
+}
+
+export function setTaskToMembers(taskId, usernameList) {
+    let i;
+    let sql = "DELETE FROM assigned_task WHERE task_id = '".concat(taskId, "';\n");
+    for (i = 0; i < usernameList.length; i++) {
+        sql = sql.concat("INSERT INTO assigned_task (task_id, username) VALUES ('",
+            taskId, "','", usernameList[i], "'", ');\n');
+
+    }
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Task assigned to members");
+    });
+}
+
+export function getTaskDependencies(taskId) {
+    let sql = "SELECT depend_on_task_id FROM task_dependencies WHERE task_id = '"
         .concat(taskId, "\'");
-        console.log(sql);
-        con.query(sql, function (err, result) {
-            	if (err) throw err;
-		let id_list = [];
-		for(let i = 0; i < result.length; i++){
-			id_list.push(result[i].task_id);
-		}
-		console.log(id_list);
-      		return id_list;
-        });
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        let id_list = [];
+        for (let i = 0; i < result.length; i++) {
+            id_list.push(result[i].depend_on_task_id);
+        }
+        console.log(id_list);
+        return id_list;
+    });
 }
 
+export function updateTaskState(taskId, state) {
+    var sql = "UPDATE task SET state = ".concat("'", state, "'", " WHERE id = '", taskId, "'");
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result.affectedRows + " record(s) updated");
+    });
+}
 
-//INSERT INTO task (id, project_id, name, description, state, start_date, realisation_time, description_of_done) VALUES (NULL, '2', 'rayan', 'blabla', 'TODO', '2019-11-06', '5', 'To finish :\r\n- First\r\n- Second');
+export function deleteTask(taskId) {
+    let sql = "DELETE FROM task WHERE id = '"
+        .concat(taskId, "'");
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Issue removed");
+    });
+}
 
+// /!\ Pas fini
+export function setTaskChecklist(taskId, checklist) {
+    let i;
+    let sql = "DELETE FROM task_checklist WHERE task_id = '".concat(taskId, "';\n");
+    for (i = 0; i < checklist.length; i++) {
+        //TODO: Finir ça
+        sql = sql.concat("INSERT INTO assigned_task (task_id, description, is_done) VALUES ('",
+            taskId, "','",
+            taskId, "','",
+            usernameList[i], "'",
+            ');\n');
 
+    }
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Task assigned to members");
+    });
+}
 
-
-
+export function deleteChecklistOfTask(taskId) {
+    let sql = "DELETE FROM task_checklist WHERE task_id = '".concat(taskId, "';\n");
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Task assigned to members");
+    });
+}
