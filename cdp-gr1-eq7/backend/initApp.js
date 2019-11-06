28a0704a673c924c7efdcbdf2b9148dc738bcd6a
RayanKheloufi <pikaray00@gmail.com>
2019-11-06 22:46:50 +0100

    Sign in and Sign Up views

--- cdp-gr1-eq7/backend/initApp.js
+++ cdp-gr1-eq7/backend/initApp.js
@@ -9,12 +9,16 @@ let bodyParser = require('body-parser')
 const index = require('./index')
 const listProjects = require('./listProjects') // TODO : move to signIn
 const listTasks = require('./listTasks') // TODO : move to signIn
+const signIn = require('./signIn') 
+const signUp = require('./signUp') 
 
 /* USE THE REQUIRES */
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(index.app)
 app.use(listProjects.app) // TODO : move to signIn
 app.use(listTasks.app)
+app.use(signIn.app)
+app.use(signUp.app)
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, './..', '/views'))
