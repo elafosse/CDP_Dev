d408d19f9a78c31257f68756c2d45a993786497e
elafosse <elafosse@emi.u-bordeaux1.fr>
2019-11-21 15:32:28 +0100

    test

--- test/SignInTest.spec.js
+++ test/SignInTest.spec.js
@@ -2,7 +2,7 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 describe('Index', function() {
-  this.timeout(30000)
+  this.timeout(300000)
   let driver
   let vars
   beforeEach(async function() {
