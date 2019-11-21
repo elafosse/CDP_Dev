2ca6e2aba648883b660a2de199b1ccc5b8ace2a8
elafosse <elafosse@emi.u-bordeaux1.fr>
2019-11-21 23:09:40 +0100

    test

--- test/indexWithoutSession.spec.js
+++ test/indexWithoutSession.spec.js
@@ -3,7 +3,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 
 describe('indexWithoutSession', function() {
-  this.timeout(30000)
+  this.timeout(300000)
   let driver
   let vars
   beforeEach(async function() {
