d3a9a4196e3e468fa2af190f3458b1de2758674a
jmenan33 <jimmy.menen@etu.u-bordeaux.fr>
2019-11-28 12:35:24 +0100

    (T51) issues summary functionnal

--- cdp-gr1-eq7/backend/overviewProject.js
+++ cdp-gr1-eq7/backend/overviewProject.js
@@ -31,9 +31,26 @@ const MODIFY_ISSUE_REDIRECT_URL = '/listIssues?projectId='
 
 const PROJECT_OVERVIEW_VIEW_PATH = '../views/overviewProject'
 
+function getIssueState(taskStatus) {
+  if (!taskStatus.total) {
+    return 4
+  }
+  if (taskStatus.totalDoing) {
+    return 2
+  }
+  if (taskStatus.totalToDo) {
+    return 3
+  }
+  if (taskStatus.totalDone === taskStatus.total) {
+    return 1
+  }
+  return 4
+}
+
 let projectId
 let listProjects = []
 let sess
+let projectIssuesSummary = [0, 0, 0, 0, 0]
 
 app.get(PROJECT_OVERVIEW_ROUTE, function(req, res) {
   projectId = req.query.projectId
@@ -41,24 +58,45 @@ app.get(PROJECT_OVERVIEW_ROUTE, function(req, res) {
     projectId = req.query.projectId
 
     listProjects = []
+    projectIssuesSummary = [0, 0, 0, 0, 0]
     sess = req.session
     sess.project = project
 
-    db._getProjectsOfMember(sess.username)
-      .then(listProjectsMembers => {
-        listProjectsMembers.forEach(element => {
-          listProjects.push(element)
+    db._getProjectsOfMember(sess.username).then(listProjectsMembers => {
+      listProjectsMembers.forEach(element => {
+        listProjects.push(element)
+      })
+      db._getCountIssuesProject(projectId).then(count => {
+        projectIssuesSummary[0] = count[0].total
+        db._getAllProjectIssues(projectId).then(issues => {
+          let asyncForEach = new Promise((resolve, reject) => {
+            issues.forEach((issue, index, array) => {
+              db._getCountTasksStatesFromIssues(issue.id)
+                .then(result => {
+                  let status = getIssueState(result[0])
+                  console.log(status)
+                  ++projectIssuesSummary[status]
+                })
+                .then(() => {
+                  if (index === array.length - 1) resolve()
+                })
+            })
+          })
+
+          asyncForEach.then(() => {
+            console.log(projectIssuesSummary)
+            sess.listProjects = listProjects
+            res.render(PROJECT_OVERVIEW_VIEW_PATH, {
+              session: req.session,
+              project: sess.project,
+              projectId: projectId,
+              listProjects: sess.listProjects,
+              projectIssuesSummary: projectIssuesSummary
+            })
+          })
         })
       })
-      .then(value => {
-        sess.listProjects = listProjects
-        res.render(PROJECT_OVERVIEW_VIEW_PATH, {
-          session: req.session,
-          project: sess.project,
-          projectId: projectId,
-          listProjects: sess.listProjects
-        })
-      })
+    })
   })
 })
 
