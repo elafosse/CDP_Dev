2b4d88726858b7921038ef1e3210ca25e833a3f0
elafosse <elafosse@emi.u-bordeaux1.fr>
2019-11-30 12:26:37 +0100

    tests selenium

--- cdp-gr1-eq7/testsSeleniumSprint2/js/001signUpThenSignInThenSignOut.spec.js
+++ cdp-gr1-eq7/testsSeleniumSprint2/js/001signUpThenSignInThenSignOut.spec.js
@@ -28,6 +28,6 @@ describe('001_signUpThenSignInThenSignOut', function() {
     await driver.findElement(By.id('password')).click()
     await driver.findElement(By.id('password')).sendKeys('test')
     await driver.findElement(By.css('.btn-primary')).click()
-    await driver.findElement(By.css('.btn-outline-danger')).click()
+    await driver.findElement(By.css('.btn-danger')).click()
   })
 })
